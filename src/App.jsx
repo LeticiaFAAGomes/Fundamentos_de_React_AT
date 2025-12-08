b9c95a7ecbdca7ed8610c8794b8f3051783bfb80
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2025-12-07 22:07:13 -0300

    feat: add AT.3 - estilizacao e renderizacao de objetos

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,33 @@ import Header from "./components/header/Header";
 import EnvironmentCard from "./components/header/environmentCard/EnvironmentCard";
 
 function App() {
+  const monitoramentoAmbiental = [
+    {
+      id: 1,
+      location: "São Paulo",
+      condition: "Condição Crítica",
+      isActive: false,
+      lastUpdate: "02/12/2025",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa eius vitae eligendi ut provident illo, quisquam, unde earum cumque magnam tenetur, ipsam distinctio ad quo aspernatur. Iste exercitationem natus earum!",
+    },
+    {
+      id: 2,
+      location: "Chapada Diamantina",
+      condition: "Condição Boa",
+      isActive: true,
+      lastUpdate: "08/12/2025",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa eius vitae eligendi ut provident illo, quisquam, unde earum cumque magnam tenetur, ipsam distinctio ad quo aspernatur. Iste exercitationem natus earum!",
+    },
+  ];
+
   return (
     <div className='App'>
       <Header />
       <main>
         <div className='container'>
-          <EnvironmentCard location='São Paulo' condition='Condição Crítica' isActive={false} />
-          <EnvironmentCard location='Chapada Diamantina' condition='Condição Boa' isActive={true} />
+          {monitoramentoAmbiental.map((dado, index) => {
+            return <EnvironmentCard dado={dado} key={index} />;
+          })}
         </div>
       </main>
     </div>
--- src/components/header/environmentCard/EnvironmentCard.jsx
+++ src/components/header/environmentCard/EnvironmentCard.jsx
@@ -1,21 +1,28 @@
 import styles from "./EnvironmentCard.module.css";
 
-export default function EnvironmentCard({ location, condition, isActive }) {
+export default function EnvironmentCard({ dado }) {
+  const id = dado.id;
+  const location = dado.location;
+  const condition = dado.isActive;
+  const lastUpdate = dado.lastUpdate;
+  const description = dado.description;
   let ehAtivo = definirStatusMonitoramento();
 
   function definirStatusMonitoramento() {
-    return isActive ? <span className={styles.ativo}>• Monitoramento Ativo</span> : <span className={styles.inativo}> • Monitoramento Inativo</span>;
+    return dado.isActive ? <span className={styles.ativo}>• Monitoramento Ativo</span> : <span className={styles.inativo}> • Monitoramento Inativo</span>;
   }
 
   return (
     <>
       <div className={styles.card} id='environmentCard'>
         <div className={styles.card__container}>
-          <h2>{location}</h2>
+          <h2>{dado.location}</h2>
           <div className={styles.card__detalhes}>
-            <span>● {condition}</span>
+            <span>● {dado.condition}</span>
             {ehAtivo}
+            <span>• Última Atualização: {lastUpdate}</span>
           </div>
+          <p>{description}</p>
         </div>
       </div>
     </>
